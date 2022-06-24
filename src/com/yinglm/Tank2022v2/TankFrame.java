84cdaa1ca834ecfb3a9da7412846f626ba873d97
v_win7_hc <yinglm@vip.qq.com>
2022-06-24 16:41:04 +0800

    Tank. 使用键盘控制四个方向移动

--- src/com/yinglm/Tank2022v2/TankFrame.java
+++ src/com/yinglm/Tank2022v2/TankFrame.java
@@ -6,7 +6,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class TankFrame extends Frame {
-    private int x =100;
+    private int x =100 , y =100;
+    private static final int SPEED=5;
 
     public TankFrame(){
         this.setTitle("tank war");
@@ -20,8 +21,8 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g) {
 
-        g.fillRect(x,100,50,50);
-        x++;
+        g.fillRect(x,y,50,50);
+      //  x++;
 //        System.out.println("paint");  此方法画图时自动调用
 
     }
@@ -30,12 +31,20 @@ public class TankFrame extends Frame {
 
         @Override
         public void keyPressed(KeyEvent e) {
-            super.keyPressed(e);
+            int key = e.getKeyCode();
+               switch (key){
+                   case KeyEvent.VK_LEFT : x-=SPEED; break;
+                   case KeyEvent.VK_UP : y-=SPEED; break;
+                   case KeyEvent.VK_RIGHT : x+=SPEED; break;
+                   case KeyEvent.VK_DOWN : y+=SPEED; break;
+
+
+               }
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            super.keyReleased(e);
+
         }
     }
 }
