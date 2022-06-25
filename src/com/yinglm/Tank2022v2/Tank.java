dde2980a8156ae03a2e5eeba862cd9ab7e873803
v_win7_hc <yinglm@vip.qq.com>
2022-06-25 14:50:20 +0800

    Tank.图片加方向

--- src/com/yinglm/Tank2022v2/Tank.java
+++ src/com/yinglm/Tank2022v2/Tank.java
@@ -28,13 +28,17 @@ public class Tank {
 
     public void paint(Graphics g) {
         //g.fillRect(x,y,50,50);
-        try {
-            BufferedImage tankL = ImageIO.read(Tank.class.getClassLoader().getResourceAsStream("images/GoodTank1.png"));
-            g.drawImage(tankL,x,y,null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        switch (dir){
+            case L:
+                g.drawImage(ResourceMgr.goodTankL,x,y,null);break;
+            case U:
+                g.drawImage(ResourceMgr.goodTankU,x,y,null);break;
+            case R:
+                g.drawImage(ResourceMgr.goodTankR,x,y,null);break;
+            case D:
+                g.drawImage(ResourceMgr.goodTankD,x,y,null);break;
 
+        }
         move();
     }
 
